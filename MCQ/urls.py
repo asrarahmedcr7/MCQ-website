824e6c185d448ad28f5e3b278c19dc6672c732ca
asrarahmedcr7 <asrarahmedcr7@gmail.com>
2024-09-16 22:07:21 +0530

    Added Index Page

--- MCQ/urls.py
+++ MCQ/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name = "MCQ"
+
 urlpatterns = [
-    path('', views.index.as_view(), name="index"),
-    path('/<int>:pk/quiz', views.quiz.as_view(), name="quiz"),
-    path('/<int>:pk/result', views.result.as_view(), name="result"),
+    path('home', views.index.as_view(), name="index"),
+    path('<int:pk>/quiz', views.quiz.as_view(), name="quiz"),
+    path('<int:pk>/result', views.result.as_view(), name="result"),
 ]
--- MCQ/views.py
+++ MCQ/views.py
@@ -1,10 +1,15 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from .models import Quiz, Question, Choice
 
-class index(generic.DetailView):
-    model = Quiz
+class index(generic.ListView):
+    context_object_name = "quizzes"
     template_name = 'MCQ/index.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Quiz.objects.all()
 
 class quiz(generic.DetailView):
     model = Quiz
